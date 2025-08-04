1365044226f1b1b7b16001be1fe2296ed734fb89
Uday-GGU <ukumara@my.ggu.edu>
2025-08-04 22:05:02 +0530

    Update vite.config.ts
    Removed /

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/BrightRoots/', // ✅ This is required for GitHub Pages
+  base: '/BrightRoots', // ✅ This is required for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
